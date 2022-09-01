fc4f8d52e6711ed113ab691f03022c7e0b4e10b6
indexofmayank <maimayanmanu@gmail.com>
2022-09-01 12:58:16 +0530

    mornging

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
-import useScript from '../hooks/useScript';
+import React from 'react'
 
 const JoinUs = () => {
   return (
-    useScript('https://apps.elfsight.com/p/platform.js'),
-    (<div className="elfsight-app-3e2d39e4-4d10-441b-a562-ad49aaca25db"></div>)
-  );
-};
+    <div>JoinUs</div>
+  )
+}
 
-export default JoinUs;
+export default JoinUs
