1e59bf9933afb3f3df623886b10f137a63c34cb0
indexofmayank <maimayanmanu@gmail.com>
2022-09-01 13:34:28 +0530

    hopeDomeNow

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -1,9 +1,11 @@
+eslint no-use-before-define: 0   // --> OFF
+
 import React from 'react'
 
 const JoinUs = () => {
   return (
     <div>JoinUs</div>
-  )
-}
+  );
+};
 
-export default JoinUs
+export default JoinUs;
