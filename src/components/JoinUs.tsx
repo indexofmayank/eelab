ffced2f9e2a657725113a69065814c5864519d95
indexofmayank <maimayanmanu@gmail.com>
2022-09-01 17:41:50 +0530

    kyabollu

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -4,7 +4,10 @@ import useScript from '../hooks/UseScript';
 const JoinUs = () => {
   return (
     useScript('https://apps.elfsight.com/p/platform.js'),
+    <div id='joinUs'>
+    
     <div className="elfsight-app-3e2d39e4-4d10-441b-a562-ad49aaca25db"></div>
+    </div>
   );
 };
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 export const chunkArr = <T>(arr: Array<T>, chunkSize: number) => {
-  const chunked = [];
+  const chunked: any[] = [];
   for (let i = 0; i < arr.length; i += chunkSize) {
     chunked.push(arr.slice(i, i + chunkSize));
   }
